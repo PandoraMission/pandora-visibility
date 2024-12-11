022ee6b6770939f54e7a71c54f91599cd5e97630
mrtommyb <barclay.astro@gmail.com>
2024-12-11 15:57:31 -0500

    updating tests

--- tests/test_import.py
+++ tests/test_import.py
@@ -2,17 +2,23 @@ from pandoravisibility import Visibility
 
 def test_visibility():
     # Example TLE lines (replace with actual TLE data)
-    line1 = "1 25544U 98067A   21036.62522304  .00001264  00000-0  31404-4 0  9993"
-    line2 = "2 25544  51.6411  12.6452 0001589  96.1237 263.1417 15.49318588339242"
+    line1 = "1 99152U 25037A   25216.00000000 .000000000  00000+0  00000-0 0   427"
+    line2 = "2 99152  97.7015  44.6980 0000010   0.1045   0.0000 14.89350717  1230"
 
-    tle = Visibility(line1, line2)
+    vis = Visibility(line1, line2)
 
     # Test get_period method
-    period = tle.get_period()
-    assert period > 0
+    period = vis.get_period()
+    assert period.value > 96.68
+    assert period.value < 96.69
 
     # Test get_state method with a time input
     from astropy.time import Time
     time = Time("2025-01-01T00:00:00")
-    state = tle.get_state(time)
+    state = vis.get_state(time)
     assert state is not None
+
+    from astropy.constants import R_earth
+    from astropy import units as u
+    assert (vis.tle.a * u.earthRad - R_earth).to(u.km).value > 596
+    assert (vis.tle.a * u.earthRad - R_earth).to(u.km).value < 597
