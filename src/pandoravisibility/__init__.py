5a2012e80909cd98002f85ec9e16cd71c72d9208
mrtommyb <barclay.astro@gmail.com>
2024-12-30 15:38:51 -0500

    flake8 fix

--- src/pandoravisibility/__init__.py
+++ src/pandoravisibility/__init__.py
@@ -4,6 +4,6 @@ import os
 PACKAGEDIR = os.path.abspath(os.path.dirname(__file__))
 TESTDIR = "/".join(PACKAGEDIR.split("/")[:-2]) + "/tests/"
 
-from .visibility import Visibility
+from .visibility import Visibility  # noqa: E402, F401
 
 __all__ = ["Visibility"]
