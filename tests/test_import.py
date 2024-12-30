a9f29f3e67cad1fd88e8f838dd2df30ec98d8320
mrtommyb <barclay.astro@gmail.com>
2024-12-30 16:19:05 -0500

    adding test to run the observing constraints

--- tests/test_import.py
+++ tests/test_import.py
@@ -19,7 +19,38 @@ def test_visibility():
     state = vis.get_state(time)
     assert state is not None
 
+    # test satnum
+    assert int(vis.tle.satnum) == 99152
+
     from astropy.constants import R_earth
     from astropy import units as u
     assert (vis.tle.a * u.earthRad - R_earth).to(u.km).value > 596
     assert (vis.tle.a * u.earthRad - R_earth).to(u.km).value < 597
+
+def test_target():
+    from astropy.time import Time, TimeDelta
+
+    # Example TLE lines (replace with actual TLE data)
+    line1 = "1 99152U 25037A   25216.00000000 .000000000  00000+0  00000-0 0   427"
+    line2 = "2 99152  97.7015  44.6980 0000010   0.1045   0.0000 14.89350717  1230"
+
+    vis = Visibility(line1, line2)
+
+    tstart = Time("2025-01-01T00:00:00.000")
+    tstop = Time("2025-01-02T00:00:00.000")  # Example stop time
+
+    dt = TimeDelta((1 / 144) * u.day, format="jd")  # 10 minute in Julian days
+    times = tstart + np.arange(0, (tstop - tstart).jd, dt.jd)  # Create time array
+
+    # using Capella because it is visible in the time period
+    target_coord = SkyCoord(79.17305002, 45.99514569, frame="icrs", unit="deg")
+    targ_vis = vis.get_visibility(target_coord, times)
+
+    assert int(targ_vis.shape[0]) == 144
+    assert targ_vis.astype(int).sum() == 85
+
+
+
+
+
+
