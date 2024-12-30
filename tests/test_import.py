e66faf540eab643996ca0ccc908e863630ad0847
mrtommyb <barclay.astro@gmail.com>
2024-12-30 16:22:44 -0500

    fixing tests

--- tests/test_import.py
+++ tests/test_import.py
@@ -28,23 +28,23 @@ def test_visibility():
     assert (vis.tle.a * u.earthRad - R_earth).to(u.km).value < 597
 
 def test_target():
-    from astropy.time import Time, TimeDelta
-    import numpy as np
-    from astropy import units as u
-
     # Example TLE lines (replace with actual TLE data)
     line1 = "1 99152U 25037A   25216.00000000 .000000000  00000+0  00000-0 0   427"
     line2 = "2 99152  97.7015  44.6980 0000010   0.1045   0.0000 14.89350717  1230"
 
     vis = Visibility(line1, line2)
 
+    from astropy.time import Time, TimeDelta
     tstart = Time("2025-01-01T00:00:00.000")
     tstop = Time("2025-01-02T00:00:00.000")  # Example stop time
 
+    import numpy as np
+    from astropy import units as u
     dt = TimeDelta((1 / 144) * u.day, format="jd")  # 10 minute in Julian days
     times = tstart + np.arange(0, (tstop - tstart).jd, dt.jd)  # Create time array
 
     # using Capella because it is visible in the time period
+    from astropy.coordinates import SkyCoord
     target_coord = SkyCoord(79.17305002, 45.99514569, frame="icrs", unit="deg")
     targ_vis = vis.get_visibility(target_coord, times)
 
