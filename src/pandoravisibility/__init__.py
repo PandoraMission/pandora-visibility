fcf21a9df6e6468e52194e08fc6fb1d2efd5fb9e
Joe P. Renaud <joe.p.renaud@gmail.com>
2026-09-09 12:35:30 -0400

    removes unused code

--- src/pandoravisibility/__init__.py
+++ src/pandoravisibility/__init__.py
@@ -8,15 +8,10 @@ import importlib.metadata
 __version__ = importlib.metadata.version("pandoravisibility")
 version = __version__
 
-from .utils import analyze_target_yearly_visibility  # noqa: E402, F401
 from .utils import (
     analyze_yearly_visibility,
     calculate_visibility_statistics,
-    export_visibility_periods,
     find_continuous_periods,
-    find_optimal_observation_windows,
-    plot_observation_windows,
-    plot_visibility_summary,
     plot_yearly_visibility,
 )
 from .visibility import Visibility  # noqa: E402, F401
@@ -26,10 +21,5 @@ __all__ = [
     "analyze_yearly_visibility",
     "find_continuous_periods",
     "calculate_visibility_statistics",
-    "find_optimal_observation_windows",
-    "export_visibility_periods",
-    "analyze_target_yearly_visibility",
     "plot_yearly_visibility",
-    "plot_visibility_summary",
-    "plot_observation_windows",
 ]
